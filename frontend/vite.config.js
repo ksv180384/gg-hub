38ba4496db41c8da109ad783f8e297bc60980531
s.kotelevsky <s.kotelevsky@sfbtech.org>
2025-02-18 17:01:54 +0300

    Настройка nginx и docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,11 +8,21 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 3008,
+    hmr: {
+      host: 'gg-hub.local',
+      clientPort: 3010
+    },
+    // Добавляем разрешенные хосты
+    allowedHosts: [
+      'localhost',
+      'gg-hub.local',
+      '.gg-hub.local', // Разрешаем все субдомены
+    ],
     proxy: {
       '/api/v1': {
-        target: 'http://gg-nginx',
+        target: 'http://gg-nginx:3010',
         changeOrigin: true,
         secure: false,
         // logLevel: 'debug'
