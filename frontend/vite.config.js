6c1b20b8ca7a7f40425f51ce5e005226fbf66926
s.kotelevsky <s.kotelevsky@sfbtech.org>
2025-02-19 16:59:39 +0300

    Настройка nginx и docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,37 +6,44 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 3008,
-    hmr: {
-      host: 'gg-hub.ru',
-      clientPort: 80
-    },
-    // Добавляем разрешенные хосты
-    allowedHosts: [
-      'localhost',
-      'gg-hub.local',
-      '.gg-hub.local', // Разрешаем все субдомены
-    ],
-    proxy: {
-      '/api/v1': {
-        target: 'http://gg-nginx:80',
-        changeOrigin: true,
-        secure: false,
-        // logLevel: 'debug'
+export default defineConfig(({ mode }) => {
+
+  console.log(mode);
+
+  const env = loadEnv(mode, process.cwd() + '/frontend');
+
+  return {
+    server: {
+      host: '0.0.0.0',
+      port: 3008,
+      hmr: {
+        host: 'gg-hub.ru',
+        clientPort: 80
       },
-    }
-  },
-  plugins: [
-    tailwindcss(),
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      // Добавляем разрешенные хосты
+      allowedHosts: [
+        'localhost',
+        'gg-hub.local',
+        '.gg-hub.local', // Разрешаем все субдомены
+      ],
+      proxy: {
+        '/api/v1': {
+          target: 'http://gg-nginx:80',
+          changeOrigin: true,
+          secure: false,
+          // logLevel: 'debug'
+        },
+      }
     },
-  },
-})
+    plugins: [
+      tailwindcss(),
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
+    },
+  }
+});
